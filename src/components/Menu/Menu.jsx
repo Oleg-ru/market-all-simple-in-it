5a654d3416a902ec0dc279f62ae3da459e61b2ff
Oleg <oleg_09-95@mail.ru>
2025-12-25 20:02:09 +0300

    Video #5 Разобрал контекст функций. 1) Обычная не видит this контекст класса (нужно .bind(this)) 2) стрелочная в конструкторе -> видит 3) Стрелочная в классе -> видит

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,17 +6,37 @@ import statisticsIcon from "../../assets/icons/statistics.svg"
 import singoutIcon from "../../assets/icons/signout.svg"
 
 class Menu extends React.Component {
+
+    test = "test"
+
+    constructor(props) {
+        super(props);
+        
+        this.onMenuItemClick2 = () => {
+            console.log("click 2", this.test)
+        }
+    }
+
+
+    onMenuItemClick() {
+        console.log("click 1", this.test)
+    }
+
+    onMenuItemClick3 = () => {
+        console.log("click 3", this.test)
+    }
+
     render() {
         return (
             <div className="menu-container">
                 <div className="menu-items">
-                    <div className="menu-item">
+                    <div className="menu-item" onClick={this.onMenuItemClick.bind(this)}>
                         <img className="menu-item-icon" src={homeIcon} alt=""/>
                     </div>
-                    <div className="menu-item">
+                    <div className="menu-item" onClick={this.onMenuItemClick2}>
                         <img className="menu-item-icon" src={shoppingIcon} alt=""/>
                     </div>
-                    <div className="menu-item">
+                    <div className="menu-item" onClick={this.onMenuItemClick3}>
                         <img className="menu-item-icon" src={statisticsIcon} alt=""/>
                     </div>
                 </div>
