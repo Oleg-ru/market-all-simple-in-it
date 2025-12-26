4b6ea59b9ec497da8e3c77827556b742bdf01542
Oleg <oleg_09-95@mail.ru>
2025-12-26 15:39:54 +0300

    Video #7 state для добавления Card

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,16 +4,31 @@ import Shopping from "../pages/Main/Tabs/Shopping/Shopping.jsx";
 
 class Content extends React.Component {
 
-    cards = [
-        {price: "5.99", cardName: "book"},
-        {price: "1100.00", cardName: "smartphone"},
-        {price: "1500.50", cardName: "laptop"},
-    ]
+    state = {
+        cards: [
+            {price: "5.99", cardName: "book"},
+            {price: "1100.00", cardName: "smartphone"},
+            {price: "1500.50", cardName: "laptop"},
+        ]
+    }
+    
+    addCard = () => {
+
+        const newCard = this.state.cards.concat({
+            price: "0.0",
+            cardName: "product_test"
+        });
+
+        this.setState({
+            cards: newCard
+        })
+    }
 
     render() {
         return (
             <div className="content-container">
-                <Shopping name="test" cards={this.cards}/>
+                <button onClick={this.addCard}>test</button>
+                <Shopping name="test" cards={this.state.cards}/>
             </div>
         )
     }
