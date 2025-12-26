a685b88b5ba6d63b0ea41907cc1f078faa2e768b
Oleg <oleg_09-95@mail.ru>
2025-12-26 16:17:33 +0300

    Video #8 Добавлена фича выделения товара по клику корзины. Слушал по Fiber и FiberNode  и обновлению компонента

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,34 @@
 import React, {Component} from "react";
 import "./Card.css"
 import cardIcon from "../../assets/icons/book.svg"
+import addCardIcon from "../../assets/icons/addCard.svg"
 
 export class Card extends Component {
+
+    state = {
+        isAdded: false
+    }
+
+    toggleIsAdded = () => {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                isAdded: !this.state.isAdded
+            }
+        })
+    }
+
     render() {
         const {price, cardName} = this.props;
 
+        const cardClass = this.state.isAdded
+            ? "card-container card-violet-background"
+            : "card-container card-orange-background";
+
         return (
-            <div className="card-container">
-                <img src={cardIcon} alt="book" className="card-icon"/>
+            <div className={cardClass}>
+                <img className="card-add-icon" onClick={this.toggleIsAdded}  src={addCardIcon} alt="add-card" />
+                <img className="card-icon"  src={cardIcon} alt="book" />
                 <div className="card-price">{price}</div>
                 <div className="card-name">{cardName}</div>
             </div>
